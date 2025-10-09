a71aa14a4008d052fcf652c1f0f23e4c0a97d573
hjpatch27 <hjpatch27@vt.edu>
2025-10-09 17:27:53 -0400

    Testing Mutation Coverage

--- src/GISTest.java
+++ src/GISTest.java
@@ -258,19 +258,20 @@ public class GISTest extends TestCase {
             + "2    L (101, 150)\n", it.debug());
         
         assertFuzzyEquals("L (101, 150)\nL (11, 500)", it.info("L"));
-        /**
-        assertFuzzyEquals("L", it.info(101, 150));
-        */
-        assertFuzzyEquals("Tacoma (1000, 100)", it.delete("Tacoma"));
         
+        assertFuzzyEquals("L", it.info(101, 150));
+       
+        assertFuzzyEquals("Tacoma (1000, 100)", it.delete("Tacoma"));
+        /**
         assertFuzzyEquals("3\nChicago", it.delete(100, 150));
+        *
         assertFuzzyEquals("L (101, 150)\n"
                 + "Atlanta (10, 500)\n"
                 + "Baltimore (0, 300)\n"
                 + "Washington (5, 350)\n"
                 + "L (11, 500)\n5", it.search(0, 0, 2000));
+                */
         assertFuzzyEquals("Baltimore (0, 300)\n4", it.search(0, 300, 0));
-        
         
     }   
     
